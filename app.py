f6cac0d12eb38df2387160123af1822f65b65932
TSRay911 <162695043+TSRay911@users.noreply.github.com>
2025-09-05 14:45:32 +0800

    added explicit encoding to file open

--- app.py
+++ app.py
@@ -154,7 +154,7 @@ def _visual(row):
                     return "assets/skip.json"
 
 def load_lottie_file(file_path: str):
-    with open(file_path, "r") as file:
+    with open(file_path, "r", encoding="utf-8") as file:
         return json.load(file)
 
 def map_action(action):
